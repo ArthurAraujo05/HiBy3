0dcceff989f3228799653f4c0647e96513b90f4a
Araujo <arthurguiaraujo123@gmail.com>
2025-11-11 11:25:18 -0300

    rm Front

--- ponto-web/src/main.ts
+++
@@ -1,7 +0,0 @@
-// Arquivo: ponto-web/src/main.ts
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component'; // <-- O nome da classe é AppComponent
-
-bootstrapApplication(AppComponent, appConfig) // <-- Inicia com AppComponent e appConfig
-  .catch((err) => console.error(err));
